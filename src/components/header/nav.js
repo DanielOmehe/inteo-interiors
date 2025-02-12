cd023bb1b8f85e7ae03ae7574f12f2a9a1de7562
Daniel Omehe <103520487+DanielOmehe@users.noreply.github.com>
2025-02-12 21:09:30 +0100

    Update nav.js

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -18,6 +18,25 @@ const InteoNavbar = () => {
       <Inteo />
       <MobileNav />
       <InteoItems>
+            <button
+                className="hidden sm:block text-white focus:outline-none"
+                onClick={toggleNav}
+            >
+                <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16m-7 6h7"
+                    ></path>
+                </svg>
+            </button>
         <InteoNavItems>
           {navItems.map((item, indx) => (
             <InteoNavItem key={indx} path={item.path} >
