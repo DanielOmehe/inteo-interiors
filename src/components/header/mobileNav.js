360770e58b50d205b198362a1c018dfb2bbb6512
Daniel Omehe <103520487+DanielOmehe@users.noreply.github.com>
2025-02-12 21:14:07 +0100

    Update mobileNav.js

--- src/components/header/mobileNav.js
+++ src/components/header/mobileNav.js
@@ -5,28 +5,7 @@ const MobileNav = () => {
     
 
     return (
-        <div className="sm:fixed sm:w-full sm:h-auto sm:p-12 sm:bg-black">
-            <button
-                className="text-white focus:outline-none"
-                onClick={toggleNav}
-            >
-                <svg
-                    className="w-6 h-6"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16m-7 6h7"
-                    ></path>
-                </svg>
-            </button>
-            {isOpen && (
-                <div className="absolute top-0 left-0 w-full bg-gray-800 text-white">
+        <div className="absolute top-0 left-0 w-full bg-gray-800 text-white">
                     <ul className="flex flex-col items-center space-y-4 py-4">
                         <li>
                             <a href="#home" className="hover:text-gray-400">Home</a>
@@ -42,8 +21,6 @@ const MobileNav = () => {
                         </li>
                     </ul>
                 </div>
-            )}
-        </div>
     );
 };
 
